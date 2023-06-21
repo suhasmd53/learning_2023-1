da3a8d3b4d87f0cf2ca333624d9ca059d9ed0dca
chandrashekar <chandu15661362@gmail.com>
2023-06-21 13:15:46 +0530

    prohrammer

---
+++ module1/day2/c1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int find_greatest(int num1, int num2, int num3) {
+    if (num1 >= num2 && num1 >= num3) {
+        return num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+int main() {
+    int num1, num2, num3;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    printf("Enter the third number: ");
+    scanf("%d", &num3);
+    
+    int greatest = find_greatest(num1, num2, num3);
+    printf("The greatest number is: %d\n", greatest);
+    
+    return 0;
+}
+
+
