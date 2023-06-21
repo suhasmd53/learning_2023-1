b362a6d26b590778e14e78abb145c4f8028c4a7c
chandrashekar <chandu15661362@gmail.com>
2023-06-21 12:43:11 +0530

    PROGRAM

--- module1/day1/c1.c
+++ module1/day1/c1.c
@@ -1,1 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int find_biggest_if_else(int num1, int num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+int find_biggest_ternary(int num1, int num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    int biggest_if_else = find_biggest_if_else(num1, num2);
+    int biggest_ternary = find_biggest_ternary(num1, num2);
+    
+    printf("Using if-else: The biggest number is %d\n", biggest_if_else);
+    printf("Using ternary operator: The biggest number is %d\n", biggest_ternary);
+    
+    return 0;
+}
+
